0ce18d11d86b13f3bb998a707b42bde2abdef5a3
Maala <maala@wizme.com>
2020-06-28 02:45:41 +0300

    add user orders url

--- src/config/api-urls.js
+++ src/config/api-urls.js
@@ -3,3 +3,4 @@ export const BaseUrl = "http://innoscripta-demo.us-east-1.elasticbeanstalk.com";
 export const SignInUrl = BaseUrl + "/user/sign-in";
 export const SignUpUrl = BaseUrl + "/user/sign-up";
 export const PizzaUrl = BaseUrl + "/pizza";
+export const OrdersUrl = BaseUrl + "/order/user";
