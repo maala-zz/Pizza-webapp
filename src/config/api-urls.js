ad107af20607a2f1135271218f1d86f9bfea916d
Maala <maala@wizme.com>
2020-06-27 20:40:24 +0300

    add get pizza url

--- src/config/api-urls.js
+++ src/config/api-urls.js
@@ -2,3 +2,4 @@ export const BaseUrl = "http://innoscripta-demo.us-east-1.elasticbeanstalk.com";
 
 export const SignInUrl = BaseUrl + "/user/sign-in";
 export const SignUpUrl = BaseUrl + "/user/sign-up";
+export const PizzaUrl = BaseUrl + "/pizza";
