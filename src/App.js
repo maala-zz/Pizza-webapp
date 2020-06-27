52946be73c302f0967a2f536c08c0394f86e5725
Maala <maala@wizme.com>
2020-06-27 12:16:40 +0300

    add route for auth pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./screens/login/Login";
+import Signup from "./screens/signup/Signup";
 import { connect } from "react-redux";
 
 
@@ -11,7 +12,8 @@ class App extends Component {
     return (
       <div className="App">
       <Switch>
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/signin" component={Login} />
+        <Route exact path="/signup" component={Signup} />
       </Switch>
       </div>
     );
