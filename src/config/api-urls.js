e57f10953d1c868c41be9c735741d0cacccb718c
Maala <maala@wizme.com>
2020-06-27 13:49:08 +0300

    add sign up url

--- src/config/api-urls.js
+++ src/config/api-urls.js
@@ -1,3 +1,4 @@
 export const BaseUrl = "http://innoscripta-demo.us-east-1.elasticbeanstalk.com";
 
 export const SignInUrl = BaseUrl + "/user/sign-in";
+export const SignUpUrl = BaseUrl + "/user/sign-up";
