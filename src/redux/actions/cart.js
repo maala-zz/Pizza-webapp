57ef2c7b6e136fb12298574286ae27261310b8f5
Maala <maala@wizme.com>
2020-06-28 15:05:05 +0300

    add main cart redux

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,5 +1,8 @@
 import * as ActionTypes from "./actionTypes";
 
+/*
+pizza = {quantity: number, id: pizza id, price: pizza price, name: the name of the pizza}
+*/
 export const addPizzaToCart = pizza => {
     return {
         type: ActionTypes.ADD_PIZZA_TO_CART,
@@ -7,6 +10,10 @@ export const addPizzaToCart = pizza => {
     };
 };
 
+
+/*
+pizza = {quantity: number, id: pizza id, price: pizza price, name: the name of the pizza}
+*/
 export const removePizzaToCart = pizza => {
     return {
         type: ActionTypes.REMOVE_PIZZA_FROM_CART,
@@ -14,9 +21,52 @@ export const removePizzaToCart = pizza => {
     };
 };
 
+
+/*
+pizza = {quantity: number, id: pizza id, price: pizza price, name: the name of the pizza}
+*/
 export const updatePizzaOnCartIfExists = pizza => {
     return {
         type: ActionTypes.UPDATE_PIZZA_ON_CART_IF_EXISTS,
         pizza: pizza
+    };
+};
+
+/*
+pizza = {quantity: number, id: pizza id}
+*/
+export const updatePizzaOnCartQuantityIfExists = pizza => {
+    return {
+        type: ActionTypes.UPDATE_PIZZA_ON_CART_QUANTITY,
+        pizza: pizza
+    };
+};
+
+
+export const updateOrderSerialId = orderSerialId => {
+    return {
+        type: ActionTypes.UPDATE_ORDER_SERIAL_ID,
+        orderSerialId: orderSerialId
+    };
+};
+
+export const updateOrderName = orderName => {
+    return {
+        type: ActionTypes.UPDATE_ORDER_NAME,
+        orderName: orderName
+    };
+};
+
+export const updateOrderAddress = orderAddress => {
+    return {
+        type: ActionTypes.UPDATE_ORDER_ADDRESS,
+        orderAddress: orderAddress
+    };
+};
+
+export const updateOrderDeliveryCost = deliveryCost => {
+    return {
+        type: ActionTypes.UPDATE_ORDER_DELIVERY_COST,
+        deliveryCost: deliveryCost
     };
 };
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,7 +1,12 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-    pizzaInCartArray: []
+    pizzaInCartArray: [],
+    totalOrderCost: 0,
+    orderSerialId: null,
+    name: null,
+    address: null,
+    deliveryCost: 0
 };
 
 const addPizzaToCart = (state, action) => {
@@ -10,7 +15,8 @@ const addPizzaToCart = (state, action) => {
         let pizzaInCartArrayAfterDelete = state.pizzaInCartArray.filter(item => item.id !== action.pizza.id);
         return {
             ...state,
-            pizzaInCartArray: pizzaInCartArrayAfterDelete
+            pizzaInCartArray: pizzaInCartArrayAfterDelete,
+            totalOrderCost: pizzaInCartArrayAfterDelete.reduce((total, pizza) => total + pizza.price * pizza.quantity, 0)
         }
     }
 
@@ -19,16 +25,19 @@ const addPizzaToCart = (state, action) => {
 
         let pizzaInCartArrayAfterDelete = state.pizzaInCartArray.filter(item => item.id !== action.pizza.id);
         let pizzaInCartArrayAfterAdded = [...pizzaInCartArrayAfterDelete, action.pizza];
-
+        ;
         return {
             ...state,
-            pizzaInCartArray: pizzaInCartArrayAfterAdded
+            pizzaInCartArray: pizzaInCartArrayAfterAdded,
+            totalOrderCost: pizzaInCartArrayAfterAdded.reduce((total, pizza) => total + pizza.price * pizza.quantity, 0)
         };
     }
 
+    let newPizzaInCartArray = [...state.pizzaInCartArray, action.pizza];
     return {
         ...state,
-        pizzaInCartArray: [...state.pizzaInCartArray, action.pizza]
+        pizzaInCartArray: [...state.pizzaInCartArray, action.pizza],
+        totalOrderCost: newPizzaInCartArray.reduce((total, pizza) => total + pizza.price * pizza.quantity, 0)
     };
 
 };
@@ -47,7 +56,8 @@ const updatePizzaOnCartIfExists = (state, action) => {
         let pizzaInCartArrayAfterDelete = state.pizzaInCartArray.filter(item => item.id !== action.pizza.id);
         return {
             ...state,
-            pizzaInCartArray: pizzaInCartArrayAfterDelete
+            pizzaInCartArray: pizzaInCartArrayAfterDelete,
+            totalOrderCost: pizzaInCartArrayAfterDelete.reduce((total, pizza) => total + pizza.price * pizza.quantity, 0)
         }
     }
 
@@ -59,10 +69,65 @@ const updatePizzaOnCartIfExists = (state, action) => {
 
         return {
             ...state,
-            pizzaInCartArray: pizzaInCartArrayAfterAdded
+            pizzaInCartArray: pizzaInCartArrayAfterAdded,
+            totalOrderCost: pizzaInCartArrayAfterAdded.reduce((total, pizza) => total + pizza.price * pizza.quantity, 0)
         };
     }
     return state;
+};
+
+const updatePizzaOnCartQuantityIfExists = (state, action) => {
+
+    if (action.pizza.quantity == "0") {
+        let pizzaInCartArrayAfterDelete = state.pizzaInCartArray.filter(item => item.id !== action.pizza.id);
+        return {
+            ...state,
+            pizzaInCartArray: pizzaInCartArrayAfterDelete,
+            totalOrderCost: pizzaInCartArrayAfterDelete.reduce((total, pizza) => total + pizza.price * pizza.quantity, 0)
+        }
+    }
+
+    let exists = state.pizzaInCartArray.some(item => action.pizza.id === item.id);
+    if (exists) {
+        let objectToUpdate = state.pizzaInCartArray.filter(item => item.id === action.pizza.id)[0];
+        let oldQuantity = objectToUpdate.quantity;
+        Object.assign(objectToUpdate, { quantity: action.pizza.quantity });
+        let difference = (action.pizza.quantity - oldQuantity) * objectToUpdate.price;
+        return {
+            ...state,
+            totalOrderCost: state.totalOrderCost + difference
+        }
+
+    }
+    return state;
+};
+
+const updateOrderSerialId = (state, action) => {
+    return {
+        ...state,
+        orderSerialId: action.orderSerialId,
+    };
+};
+
+const updateOrderName = (state, action) => {
+    return {
+        ...state,
+        name: action.orderName,
+    };
+};
+
+const updateOrderAddress = (state, action) => {
+    return {
+        ...state,
+        address: action.orderAddress,
+    };
+};
+
+const updateOrderDeliveryCost = (state, action) => {
+    return {
+        ...state,
+        deliveryCost: action.deliveryCost,
+    };
 };
 
 const CartReducer = (state = initialState, action) => {
@@ -73,6 +138,16 @@ const CartReducer = (state = initialState, action) => {
             return removePizzaToCart(state, action);
         case actionTypes.UPDATE_PIZZA_ON_CART_IF_EXISTS:
             return updatePizzaOnCartIfExists(state, action);
+        case actionTypes.UPDATE_PIZZA_ON_CART_QUANTITY:
+            return updatePizzaOnCartQuantityIfExists(state, action);
+        case actionTypes.UPDATE_ORDER_SERIAL_ID:
+            return updateOrderSerialId(state, action);
+        case actionTypes.UPDATE_ORDER_NAME:
+            return updateOrderName(state, action);
+        case actionTypes.UPDATE_ORDER_ADDRESS:
+            return updateOrderAddress(state, action);
+        case actionTypes.UPDATE_ORDER_DELIVERY_COST:
+            return updateOrderDeliveryCost(state, action);
         default:
             return state;
     }
