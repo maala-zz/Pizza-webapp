b3d18cabc8e62e7403c1041947328e75706fb89e
Maala <maala@wizme.com>
2020-06-28 15:05:36 +0300

    add cart page route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Login from "./screens/login/Login";
 import Signup from "./screens/signup/Signup";
 import Home from "./screens/home/Home";
+import Cart from "./screens/cart/Cart";
 import { connect } from "react-redux";
 
 
@@ -12,11 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Switch>
-      <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/cart" component={Cart} />
+        </Switch>
       </div>
     );
   }
