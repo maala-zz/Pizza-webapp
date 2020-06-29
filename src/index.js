e4b85907b0961257df4ba18765ba0221ce499610
Maala <maala@wizme.com>
2020-06-29 12:21:51 +0300

    migrate to yarn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
