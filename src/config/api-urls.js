ea77cbadbb436cde90a13e48c08a77947c7e074e
Maala <maala@wizme.com>
2020-06-30 09:20:38 +0300

    change api base url

--- src/config/api-urls.js
+++ src/config/api-urls.js
@@ -1,4 +1,4 @@
-export const BaseUrl = "http://innoscripta-demo.us-east-1.elasticbeanstalk.com";
+export const BaseUrl = "https://vast-scrubland-62691.herokuapp.com";
 
 export const SignInUrl = BaseUrl + "/user/sign-in";
 export const SignUpUrl = BaseUrl + "/user/sign-up";
