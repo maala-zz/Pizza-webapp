682d1bf3154f9e62f7506c5675984d093ec0f6b6
Maala <maala@wizme.com>
2020-07-01 21:49:07 +0300

    let the card take parent styles

--- src/components/pizzaCard/styles.js
+++ src/components/pizzaCard/styles.js
@@ -3,9 +3,6 @@ import { withStyles } from "@material-ui/core/styles";
 import { red } from '@material-ui/core/colors';
 export const styles = theme => ({
     root: {
-        maxWidth: "500",
-        minWidth: "500",
-        minHeight: "500",
         marginTop: "3%",
         marginLeft: "10%"
     },
